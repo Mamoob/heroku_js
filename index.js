316652b51429f3e8815ac2fa2bbc13be1fdbf178
Седов Дмитрий Валерьевич <sedov@nita-farm.ru>
2019-06-20 09:13:03 +0400

    test1

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '642010118:AAENJ-XUKCA5n0TImQKhFKvutbko2eG5vGI'
 const bot = new TelegramBot(TOKEN, {polling:true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from Heroku, bot says:"Hi,"' + {msg.from.first_name})
+    bot.sendMessage(msg.chat.id, 'Hello from Heroku, bot says:"Hi,"' + ${msg.from.first_name})
 })
